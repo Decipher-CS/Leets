5bc396829445ede1e8d8f1e80d9b3727270a4030
de-sipher <0mu5sfm4@anonaddy.me>
2022-06-24 18:24:34 +0530

    94 recursive done.

--- [94]Binary_Tree_Inorder_Traversal.py
+++ [94]Binary_Tree_Inorder_Traversal.py
@@ -7,20 +7,50 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
-class Solution:
-    def inorderTraversal(self, root: Optional[TreeNode]):
-        result = []
-        def inorder(node):
-            if not node:
-                return None
-            inorder(node.left)
-            result.append(node.val)
-            inorder(node.right)
-            return 
+
+# Recursive Solution
+# class Solution:
+#     def inorderTraversal(self, root: Optional[TreeNode]):
+#         result = []
+#         def inorder(node):
+#             if not node:
+#                 return None
+#             inorder(node.left)
+#             result.append(node.val)
+#             inorder(node.right)
+#             return 
             
         
-        inorder(root)
+#         inorder(root)
+#         return result
+
+# Iterative Solution
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]):
+        if not root:
+            return []
+        inorder = [root]
+        result = []
+        i = 0
+        ptr = inorder[i]
+        hp = {}
+        while 1:
+            if ptr.left:
+                inorder.insert(i, ptr.left)
+            if ptr.right:
+                inorder.insert(i+2, ptr.right)
+            hp[ptr] = ptr
+            try:
+                ptr, *rest = [x for x in inorder if x not in hp.keys()]
+            except ValueError:
+                break
+            i = inorder.index(ptr)
+            
+        result = [node.val for node in inorder]
         return result
 
 head = TreeNode(1, None, TreeNode(2, TreeNode(3), None))
 print(Solution().inorderTraversal(head))
+# hp = {1:1}
+# val, *val2 = [i for i in [1,2,3] if i not in hp.keys()]
+# print(val, val2)

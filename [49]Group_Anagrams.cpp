b8b1ad0116a4779b6496523eea2b23dc009995a7
de-sipher <0mu5sfm4@anonaddy.me>
2022-06-28 20:02:24 +0530

    49 current version works for relatively small set of data. Fails for larger once. Need faster solution.

--- [49]Group_Anagrams.cpp
+++ [49]Group_Anagrams.cpp
@@ -7,21 +7,25 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> result = {};
         vector<string> temp_res;
-
-        for (auto it = begin(strs); it!=end(strs); ++it){
-            temp_res.push_back(*it);
-            for (auto itj = begin(strs); itj != end(strs); ++itj){
-                if (*it == *itj){
-                    continue;
-                } 
-                if (isAnagram((*it) , *itj)){
-                    temp_res.push_back(*itj);
-                    strs.erase(itj);
-                }
+        while (strs.size() > 0)
+        {
+            string current_elem = strs.at(strs.size()-1);
+            temp_res.push_back(current_elem);
+            strs.pop_back();
+            for (int i = 0; i < strs.size(); i=i)
+            {
+                if (isAnagram(current_elem, strs[i]))
+                {
+                    temp_res.push_back(strs[i]);
+                    strs.erase(strs.begin()+i);
+                } else
+                    ++i;
             }
             result.push_back(temp_res);
             temp_res.clear();
         }
+
+
         return result;
     }
 
@@ -30,11 +34,11 @@ public:
         if (str1.length() != str2.length())
             return false;
         
+        int f;
         for (int i = 0; i< str1.length(); i++){
-            int f;
             if (  (f = str2.find(str1[i]))  == -1  )
                 return false;
-            str2.erase(f,f);
+            str2.erase(str2.begin()+f);
         }
         return true;
     }
@@ -42,7 +46,8 @@ public:
 
 int main(void){
     vector<string> strs = {"eat","tea","tan","ate","nat","bat"};
-    vector<vector<string>> result = {{"bat"},{"nat","tan"},{"ate","eat","tea"}};
+    // vector<string> strs = {"a","a","a"};
+    // vector<string> strs = {"abbbbbbbbbbb","aaaaaaaaaaab"};
     vector<vector<string>> sol = Solution().groupAnagrams(strs);
     for (auto strings : sol){
         for (auto str : strings){
@@ -50,19 +55,8 @@ int main(void){
         }
         cout << endl;
     }
-    // string s = "hello";
-    // cout << typeid(s).name() << endl;
-    // for (auto it = begin(strs);  it != end(strs); ++it){
-    //     cout << typeid(string(*it)).name() << endl;
-    // }
-
-    // cout << endl;
-    // cout << endl;
-    // cout << endl;
-
-    // for (auto str : strs){
-    //     cout << typeid(str).name() << endl;
-    // }
-
+    // string str1 = strs[0];
+    // string str2 = strs[3];
+    // cout << Solution().isAnagram(str2, str1) << endl;
     return 0;
 }

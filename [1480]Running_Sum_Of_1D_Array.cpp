c3a2b7f4e9b32cc98291c497a652e850d4802ac6
de-sipher <0mu5sfm4@anonaddy.me>
2022-07-01 20:50:46 +0530

    238 done.

--- [1480]Running_Sum_Of_1D_Array.cpp
+++ [1480]Running_Sum_Of_1D_Array.cpp
@@ -5,21 +5,14 @@
 
 using namespace std;
 
-// TODO //
-// Try Recursion //
-
 class Solution {
 public:
     // ITERATIVE SOLUTION
     vector<int> result;
     vector<int> runningSum(vector<int>& nums) {
         vector<int> result;
-        for (int i = 0; i < nums.size() ; ++i){
-            // cout << nums[i] << endl;
-            if (i==0){
-                result.push_back(0 + nums[i]);
-            } else
-                result.push_back(result[i-1] + nums[i]);
+        for (int i = 1; i < nums.size() ; ++i){
+            result.push_back(result[i-1] + nums[i]);
         }
         for (auto res : result)
             cout << res << endl;
@@ -41,7 +34,7 @@ public:
 
 int main(){
     vector<int> nums = {1,2,3,4};
-    vector<int> result = Solution().runningSum_recursion(nums);
+    vector<int> result = Solution().runningSum(nums);
     for (auto res :result )
         cout << res << endl;
     return 0;
--- [238]Product_Of_Array_Except_Self.cpp
+++ [238]Product_Of_Array_Except_Self.cpp
@@ -8,24 +8,23 @@ using namespace std;
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        unordered_map<int, int> prefix_storage;
+        unordered_map<int, int> prefix_storage; 
         unordered_map<int, int> postfix_storage;
         vector<int> sample_array;
 
+        int arr_last = nums.size()-1;
+        prefix_storage[0] = nums[0];
+        postfix_storage[arr_last] = nums[arr_last];
 
+        for (int i=1; i < nums.size() ; ++i)   prefix_storage[i] = nums[i] * prefix_storage[i-1];
+        for (int i=arr_last-1; i >= 0 ; --i)   postfix_storage[i] = nums[i] * postfix_storage[i+1];
 
-                    // TESTING PREFIX //
-        for (int i=0; i < nums.size() ; ++i){
-            if (i == 0){
-                sample_array.push_back(nums[i]);
-            } else
-                sample_array.push_back(  nums[i] * sample_array[i-1]  );
-        }
+        for (int i=1; i<arr_last; ++i)   nums[i] = prefix_storage[i-1] * postfix_storage[i+1];
 
-        for (auto num : sample_array)
-            cout << num << endl;
+        nums[0] = postfix_storage[1];
+        nums[arr_last] = prefix_storage[arr_last-1];
 
-        return {1,2,3};
+        return nums;
     }
 };
 

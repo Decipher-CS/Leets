b075a4c6ad30199dbe2847f326ff780ee2e76764
de-sipher <0mu5sfm4@anonaddy.me>
2022-07-02 23:27:55 +0530

    128 done with better and faster solution.

--- [128]Longest_Consecutive-Sequence.cpp
+++ [128]Longest_Consecutive-Sequence.cpp
@@ -3,6 +3,8 @@
 #include<map>
 #include<unordered_map>
 #include<algorithm>
+#include<set>
+#include<unordered_set>
 
 using namespace std;
 
@@ -10,36 +12,30 @@ using namespace std;
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        int result = 1;
-        int diff = 1;
-        if (!nums.size()) return 0;
-        sort(begin(nums), end(nums));
-        for (auto it : nums)
-            cout << it << endl;
+        unordered_set<int> nums_set(nums.begin(), nums.end());
+        int seq_size;
+        int result = 0;
 
-        for (auto num = nums.begin(); num != nums.end()-1; ++num){
-            if ( (*(num+1)) == (*num) ) continue;
-            if (   ( (*(num+1)) == ((*num)+1) )   ) {
-                ++diff;
-            } else {
-                diff = 1;
-            }
-            if (diff > result){
-                result = diff;
-            }
 
+        for (auto num : nums_set){
+            seq_size = 0;
+            if (nums_set.find(num-1) != end(nums_set)) continue;
+            while (nums_set.find(num+seq_size) != end(nums_set)) { seq_size++; }
+            result = (seq_size > result) ? seq_size : result ;
         }
+        
         return result;
     }
-
 };
 
 
 int main(){
-    // vector<int> nums = {100,4,200,1,3,2}; // sol => 4
+    vector<int> nums = {100,4,200,1,3,2}; // sol => 4
     // vector<int> nums = {0,3,7,2,5,8,4,6,0,1}; //sol => 8
-    vector<int> nums = {1,2,0,1}; // sol => 3
+    // vector<int> nums = {1,2,0,1}; // sol => 3
     int sol = Solution().longestConsecutive(nums);
     cout << "\nSolution is : " << sol <<endl;
+
+
     return 0;
 }

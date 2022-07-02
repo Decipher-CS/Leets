b5dd21bed00b8ae28f864918109b00a0f028859a
de-sipher <0mu5sfm4@anonaddy.me>
2022-07-02 20:41:28 +0530

    238 v2. faster and about 4x faster. Less lines of code.

--- [238]Product_Of_Array_Except_Self.cpp
+++ [238]Product_Of_Array_Except_Self.cpp
@@ -8,23 +8,21 @@ using namespace std;
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        unordered_map<int, int> prefix_storage; 
-        unordered_map<int, int> postfix_storage;
-        vector<int> sample_array;
+        int postfix = 1;
+        int prefix = 1;
+        vector<int> result(nums.size(), 1) ;
 
-        int arr_last = nums.size()-1;
-        prefix_storage[0] = nums[0];
-        postfix_storage[arr_last] = nums[arr_last];
+        for (int res=0 , num=0; num < nums.size() ; ++num, ++res){
+            result[res] *= prefix;
+            prefix *= nums[num];
+        }
 
-        for (int i=1; i < nums.size() ; ++i)   prefix_storage[i] = nums[i] * prefix_storage[i-1];
-        for (int i=arr_last-1; i >= 0 ; --i)   postfix_storage[i] = nums[i] * postfix_storage[i+1];
+        for (int res = nums.size()-1, num = nums.size()-1; num >= 0 ; --num, --res){
+            result[res] *= postfix;
+            postfix *= nums[num];
+        }
 
-        for (int i=1; i<arr_last; ++i)   nums[i] = prefix_storage[i-1] * postfix_storage[i+1];
-
-        nums[0] = postfix_storage[1];
-        nums[arr_last] = prefix_storage[arr_last-1];
-
-        return nums;
+        return result;
     }
 };
 

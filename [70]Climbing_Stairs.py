cf4901a9f33f953f130e6e3d30f7d7252791270e
de-sipher <0mu5sfm4@anonaddy.me>
2022-07-06 22:23:43 +0530

    70 done. need better efficienty to prevent time limit exceeded error.

--- [70]Climbing_Stairs.py
+++ [70]Climbing_Stairs.py
@@ -2,21 +2,23 @@
 
 class Solution:
     def climbStairs(self, n: int):
+        global steps
         steps = 0
         def calcStairs(step: int):
             if (step == n):
-                step += 1
+                global steps
+                steps += 1
                 return 1
             elif (step > n):
                 return 0
-            
-            self.climbStairs(step + 1)
-            self.climbStairs(step + 2)
+                
+            calcStairs(step + 1)
+            calcStairs(step + 2)
             
             return 
-        calcStairs(n)
+        calcStairs(0)
         return steps
 
 
 
-print(Solution().climbStairs(5))
+print(Solution().climbStairs(4))

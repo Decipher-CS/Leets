fa26981c5d64a1a687d61075cba30828cbf71a8e
de-sipher <0mu5sfm4@anonaddy.me>
2022-07-10 23:37:38 +0530

    268 done

--- [268]Missing_Numer.py
+++ [268]Missing_Numer.py
@@ -2,7 +2,18 @@ from typing import List
 
 
 class Solution:
+        # Gauss method
     def missingNumber(self, nums: List[int]) -> int:
+        return ( ((len(nums)+1) * len(nums))  // 2 ) - sum(nums)
+
+        # XOR method
+    def missingNumber_xor(self, nums: List[int]) -> int:
+        array = [i for i in range(len(nums)+1)]
+        diff = (set(array) ^ set(nums))
+        return diff.pop()
+
+        # Comparing sorted list
+    def missingNumber_sorted(self, nums: List[int]) -> int:
         nums.sort()
         n = len(nums)
         i = 0
@@ -11,3 +22,5 @@ class Solution:
                 return i
             i+=1
         return i
+
+print(Solution().missingNumber([1,2,0,4,5,6]))

d9bbbf9bfad1af764928c3b1b4d5beeb9f4a1566
de-sipher <0mu5sfm4@anonaddy.me>
2022-07-12 00:23:48 +0530

    1011 with binary search but right value not adjusted correctely.

--- [1011]Capacity_To_Ship_Package_Within_D_Days.py
+++ [1011]Capacity_To_Ship_Package_Within_D_Days.py
@@ -3,25 +3,39 @@ from typing import List
 
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int):
-        min_weight = max(weights)
-        least_weight_capacity = min_weight
-        total_weight = sum(weights)
-        while (least_weight_capacity <= total_weight):
-            running_sum = 0
-            d = days
-            for w in weights:
-                if ((running_sum + w) <= least_weight_capacity):
-                    running_sum += w
-                else:
-                    d -= 1
-                    running_sum = w
-            if (d > 0):
-                return least_weight_capacity
+        left = 1
+        right = 600
+        mid = 0
+        while (right >= left):
+            mid = (right + left) // 2
+            d = self.shipWithinDays_helper(weights, days, mid)
+            if not(d):
+                right -= 1
+            if (d < 0):
+                left = mid+1
             else:
-                least_weight_capacity += 1
+                right = mid-1
+        
+        return mid
+
+    def shipWithinDays_helper(self, weights, d, weight_capacity):
+        running_sum = 0
+        d -= 1
+        for w in weights:
+            if ((running_sum + w) <= weight_capacity):
+                running_sum += w
+            else:
+                d -= 1
+                running_sum = w
+        if (d == 0):
+            return 0
+        elif (d < 0):
+            return -1
+        else:
+            return 1
 
 inputs = [
-    [[1,2,3,4,5,6,7,8,9,10], 5],
+    # [[1,2,3,4,5,6,7,8,9,10], 5],
     [[3,2,2,4,1,4], 3],
 ]
 

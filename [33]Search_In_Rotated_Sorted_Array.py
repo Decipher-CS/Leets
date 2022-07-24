7a9f25c12a129a4a39b9ee827834a899fa86daa9
de-sipher <0mu5sfm4@anonaddy.me>
2022-07-24 16:02:23 +0530

    33 done

--- [33]Search_In_Rotated_Sorted_Array.py
+++ [33]Search_In_Rotated_Sorted_Array.py
@@ -6,15 +6,31 @@ class Solution:
         left = 0
         right = len(nums) - 1
         
-        while left < right:
+        while (left <= right):
             mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+
+            if (  ((nums[mid] < nums[left]) and (target < nums[left])) or ((nums[mid] > nums[left]) and (target >= nums[left]))  ):
+                if nums[mid] > target:
+                    right = mid-1
+                else:
+                    left = mid+1
+                
+            elif nums[mid] < nums[left]: #Mid is in the original array
+                right = mid-1
+                pass
+            else: #Mid is in rotated array
+                left = mid+1
             
-        
-        return left
+        return -1
 
 
 inputs = [
-    [[4,5,6,7,0,1,2], 0]
+    [[6,7,1,2,3,4,5], 6],
+    [[4,5,6,7,0,1,2], 0],
+    [[4,5,6,7,0,1,2], 3],
+    [[1], 0]
 ]
 
 for arr, target in inputs:

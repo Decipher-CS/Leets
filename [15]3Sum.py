692eaa04290c264c2e806263deca87dc6168bbc2
de-sipher <0mu5sfm4@anonaddy.me>
2022-07-29 15:38:20 +0530

    15 improved.

--- [15]3Sum.py
+++ [15]3Sum.py
@@ -8,20 +8,20 @@ class Solution:
         result = []
         left = pivot + 1
         right = len(nums)-1
+        print(nums)
 
         while pivot < len(nums)-2:
-            # if pivot != 0 and nums[pivot] == nums[pivot-1]:
-                # pivot +=1
-                # continue
+            if (pivot != 0) and (nums[pivot] == nums[pivot-1]):
+                pivot +=1
+                left += 1
+                continue
             sum = nums[pivot] + nums[left] + nums[right]
             if sum == 0:
-                if ([ nums[pivot], nums[left] , nums[right]] not in result):
-                    result.append([ nums[pivot], nums[left] , nums[right]])
-                # result.append([ nums[pivot], nums[left] , nums[right]])
-                # while left < right and nums[left] == nums[left+1]:
-                #     left +=1
-                # while left < right and nums[right] == nums[left-1]:
-                #     right -=1
+                result.append([ nums[pivot], nums[left] , nums[right]])
+                while left < right and nums[left] == nums[left+1]:
+                    left +=1
+                while left < right and nums[right] == nums[left-1]:
+                    right -=1
                 left += 1
                 right -= 1
             elif sum < 0:
@@ -61,10 +61,11 @@ class Solution:
         return result if result else []
 
 nums = [
-    [-1,0,1,2,-1,-4],
+    # [-1,0,1,2,-1,-4],
     # [0,1,1],
-    [0,0,0],
-    [3,0,-2,-1,1,2], # ans ->  [[-2,-1,3],[-2,0,2],[-1,0,1]]
+    # [0,0,0],
+    # [3,0,-2,-1,1,2], # ans ->  [[-2,-1,3],[-2,0,2],[-1,0,1]]
+    [-1,0,1,2,-1,-4,-2,-3,3,0,4], # ans ->[[-4,0,4],[-4,1,3],[-3,-1,4],[-3,0,3],[-3,1,2],[-2,-1,3],[-2,0,2],[-1,-1,2],[-1,0,1]] 
 ]
 
 for num in nums:

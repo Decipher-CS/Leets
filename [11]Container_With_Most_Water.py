23157eec23c04e75d671d2a9baa32dbec5d3cd97
de-sipher <0mu5sfm4@anonaddy.me>
2022-07-30 19:15:08 +0530

    11 done with greedy algorithm

--- [11]Container_With_Most_Water.py
+++ [11]Container_With_Most_Water.py
@@ -1,5 +1,21 @@
 class Solution:
     def maxArea(self, height):
+        left = 0
+        right = len(height)-1
+        area = 0
+        while left < right:
+            smaller = height[left] if height[left] < height[right] else height[right]
+            dist = right - left
+            area =  dist * smaller if (dist * smaller) > area else area
+            
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return area
+
+    def maxArea_BruteForce(self, height):
         dist = 1
         area = 0
         
@@ -17,6 +33,7 @@ nums = [
     [1,8,6,2,5,4,8,3,7],
     [0,0],
     [1,1],
+    [1,2,1]
 ]
 
 for num in nums:
